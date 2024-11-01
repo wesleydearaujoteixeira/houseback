a76b6ddd25bd8d3eac9392efc0f1ca14ff306f86
Wesley <wesleyaraujoteixeira98@gmail.com>
2024-11-01 01:14:15 -0300

    update

--- src/controllers/ControllerGlobal.ts
+++ src/controllers/ControllerGlobal.ts
@@ -284,8 +284,7 @@ export default class Controller {
 
     static async FazerReservas (req: Request, res: Response): Promise<any> {
         
-        const { house_id } = req.params;  
-        const { user_id } = req.params; 
+        const { house_id, user_id } = req.params;  
 
         const horaBrasilia = new Date().toLocaleString("pt-BR", {
             timeZone: "America/Sao_Paulo",
